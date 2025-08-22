0d925e2533289d080c7847ae3f775877a2f674b6
20rosh <roshanisingh2005@gmail.com>
2025-08-22 17:30:53 +0530

    Updated portfolio: added certificates, images, and modified HTML/CSS/JS

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ function toggleMenu(){
 menu.classList.toggle("open");
 icon.classList.toggle("open");
 }
+
+function showCertificate(course) {
+  const certificates = {
+    dsa: './certificates/DSA Course DEVTOWN.pdf',
+    accenture: './certificates/Accenture_completion_certificate.pdf',
+    cpp: './certificates/Core Concepts of C++ Course DEVTOWN.pdf',
+    ds: './certificates/dATA SCIENCE DevTown.pdf',
+    unstop: './certificates/Unstop Talent Park.pdf'
+  };
+  document.getElementById('certificate-frame').src = certificates[course] || '';
+  document.getElementById('certificate-modal').style.display = 'flex';
+}
+
+function closeCertificate() {
+  document.getElementById('certificate-modal').style.display = 'none';
+  document.getElementById('certificate-frame').src = '';
+}
+
